a8e92dc0652c3b70453019fe16ca6d1cb04ad387
krstie <kristietran010308@gmail.com>
2025-01-06 19:20:11 +0000

    s

--- Time.java
+++ Time.java
@@ -1,9 +1,17 @@
 public class Time
 {
   // TODO: Part A - put your attributes here
-
+  private int minutes;
+  private int hours;
+  private int seconds;
 
   // TODO: Part B - create your non-default constructor
+  public Time(int h, int m, int s)
+  {
+    this.hours = h;
+    this.minutes = m;
+    this.seconds = s;
+  }
   /**
   * Precondition: The number of hours is between 0 and 23 inclusive.
   * The number of minutes and seconds is between 0 and 59 inclusive.
@@ -11,11 +19,15 @@ public class Time
 
 
   // TODO: Part C - create your default constructor
+  public Time()
+  {
+    this(0,0,0);
+  }
 
 
   // TODO: Part D - complete the documentation for the pad method
   /**
-  * Description: 
+  * Description: The function takes the value and then adds a 0 in front when the value is less than 10. 
   * Precondition: The value is greater than 0.
   */
   private String pad(int value)
@@ -31,7 +43,8 @@ public class Time
   // TODO: Part E - complete the toString method; use the pad method as part of your solution
   public String toString()
   {
-    return null;
+    return pad.hours + ":" + pad.minutes + ":" + pad.seconds;
+    
   }
 
   // TODO: Part F - write the tick method
